48490826b425ed2531023015f24c39e7943c5b81
Islom Pardaboyev <ipardaboyev574@gmail.com>
2024-09-28 20:20:49 +0500

    init

--- app/js/index.js
+++ app/js/index.js
@@ -1,20 +1,20 @@
 "use strict";
-let firstPlayerButton = document.querySelector('#firstPlayerButton');
-let secondPlayerButton = document.querySelector('#secondPlayerButton');
+let firstPlayerButton = document.querySelector("#firstPlayerButton");
+let secondPlayerButton = document.querySelector("#secondPlayerButton");
 let player1 = {
     scores: [],
-    playerName: 1
+    playerName: 1,
 };
 let player2 = {
     scores: [],
-    playerName: 2
+    playerName: 2,
 };
-secondPlayerButton.disabled = true;
 function player1Start() {
     firstPlayerButton.disabled = true;
+    secondPlayerButton.disabled = false;
     let randomNumber = Math.floor(Math.random() * 10);
     let showScoreP1 = document.querySelector("#showScoreP1");
-    let totalScore = document.querySelector('#totalScore');
+    let totalScore = document.querySelector("#totalScore");
     if (showScoreP1) {
         showScoreP1.textContent = String(randomNumber);
         player1.scores.push(randomNumber);
@@ -23,13 +23,13 @@ function player1Start() {
         }
     }
     checkTotalScore(player1.scores.reduce((a, b) => a + b, 0), player2.scores.reduce((a, b) => a + b, 0));
-    secondPlayerButton.disabled = false;
 }
 function player2Start() {
     firstPlayerButton.disabled = false;
+    secondPlayerButton.disabled = true;
     let randomNumber = Math.floor(Math.random() * 10);
     let showScoreP2 = document.querySelector("#showScoreP2");
-    let totalScoreP2 = document.querySelector('#totalScoreP2');
+    let totalScoreP2 = document.querySelector("#totalScoreP2");
     if (showScoreP2) {
         showScoreP2.textContent = String(randomNumber);
         player2.scores.push(randomNumber);
@@ -38,34 +38,33 @@ function player2Start() {
         totalScoreP2.textContent = String(player2.scores.reduce((acc, curr) => acc + curr, 0));
     }
     checkTotalScore(player1.scores.reduce((a, b) => a + b, 0), player2.scores.reduce((a, b) => a + b, 0));
-    secondPlayerButton.disabled = true;
 }
 function checkTotalScore(p1Score, p2Score) {
     if (p1Score >= 100) {
-        document.querySelector('#modal')?.classList.add('!scale-100');
+        document.querySelector("#modal")?.classList.add("!scale-100");
         firstPlayerButton.disabled = true;
-        let playerWin = document.querySelector('#playerWin');
+        let playerWin = document.querySelector("#playerWin");
         if (playerWin) {
             playerWin.textContent = `Player ${player1.playerName} wins! 🥳`;
         }
     }
     if (p2Score >= 100) {
-        document.querySelector('#modal')?.classList.add('!scale-100');
+        document.querySelector("#modal")?.classList.add("!scale-100");
         secondPlayerButton.disabled = true;
-        let playerWin = document.querySelector('#playerWin');
+        let playerWin = document.querySelector("#playerWin");
         if (playerWin) {
             playerWin.textContent = `Player ${player2.playerName} wins! 🥳`;
         }
     }
 }
 function restartGame() {
-    document.querySelector('#modal').classList.remove('!scale-100');
+    document.querySelector("#modal").classList.remove("!scale-100");
     player1.scores = [];
     player2.scores = [];
-    document.querySelector('#showScoreP1').textContent = '';
-    document.querySelector('#showScoreP2').textContent = '';
-    document.querySelector('#totalScore').textContent = '0';
-    document.querySelector('#totalScoreP2').textContent = '0';
+    document.querySelector("#showScoreP1").textContent = "";
+    document.querySelector("#showScoreP2").textContent = "";
+    document.querySelector("#totalScore").textContent = "0";
+    document.querySelector("#totalScoreP2").textContent = "0";
     firstPlayerButton.disabled = false;
     secondPlayerButton.disabled = true;
 }
--- app/ts/index.ts
+++ app/ts/index.ts
@@ -1,81 +1,93 @@
-let firstPlayerButton = document.querySelector('#firstPlayerButton') as HTMLButtonElement;
-let secondPlayerButton = document.querySelector('#secondPlayerButton') as HTMLButtonElement;
-
+let firstPlayerButton = document.querySelector(
+  "#firstPlayerButton"
+) as HTMLButtonElement;
+let secondPlayerButton = document.querySelector(
+  "#secondPlayerButton"
+) as HTMLButtonElement;
 
 interface PlayersDetails {
   scores: number[];
-  playerName: number
+  playerName: number;
 }
 
 let player1: PlayersDetails = {
   scores: [],
-  playerName: 1
+  playerName: 1,
 };
 
 let player2: PlayersDetails = {
   scores: [],
-  playerName: 2
+  playerName: 2,
 };
-secondPlayerButton.disabled = true
 function player1Start() {
-    firstPlayerButton.disabled = true;
+  firstPlayerButton.disabled = true;
+  secondPlayerButton.disabled = false;
   let randomNumber = Math.floor(Math.random() * 10);
   let showScoreP1 = document.querySelector("#showScoreP1");
-  let totalScore = document.querySelector('#totalScore')
+  let totalScore = document.querySelector("#totalScore");
   if (showScoreP1) {
     showScoreP1.textContent = String(randomNumber);
     player1.scores.push(randomNumber);
-    if(totalScore){
-        totalScore.textContent = String(player1.scores.reduce((acc, curr) => acc + curr, 0));
+    if (totalScore) {
+      totalScore.textContent = String(
+        player1.scores.reduce((acc, curr) => acc + curr, 0)
+      );
     }
   }
-  checkTotalScore(player1.scores.reduce((a:number, b:number) => a + b, 0), player2.scores.reduce((a:number, b:number) => a + b, 0))
-  secondPlayerButton.disabled = false
+  checkTotalScore(
+    player1.scores.reduce((a: number, b: number) => a + b, 0),
+    player2.scores.reduce((a: number, b: number) => a + b, 0)
+  );
 }
 function player2Start() {
-    firstPlayerButton.disabled = false;
-    let randomNumber = Math.floor(Math.random() * 10);
-    let showScoreP2 = document.querySelector("#showScoreP2");
-    let totalScoreP2 = document.querySelector('#totalScoreP2');
-    if (showScoreP2) {
-        showScoreP2.textContent = String(randomNumber);
-        player2.scores.push(randomNumber);
-    }
-    if (totalScoreP2) {
-        totalScoreP2.textContent = String(player2.scores.reduce((acc, curr) => acc + curr, 0));
-    }
-    checkTotalScore(player1.scores.reduce((a:number, b:number) => a + b, 0), player2.scores.reduce((a:number, b:number) => a + b, 0))
-    secondPlayerButton.disabled = true
+  firstPlayerButton.disabled = false;
+  secondPlayerButton.disabled = true;
+  let randomNumber = Math.floor(Math.random() * 10);
+  let showScoreP2 = document.querySelector("#showScoreP2");
+  let totalScoreP2 = document.querySelector("#totalScoreP2");
+  if (showScoreP2) {
+    showScoreP2.textContent = String(randomNumber);
+    player2.scores.push(randomNumber);
+  }
+  if (totalScoreP2) {
+    totalScoreP2.textContent = String(
+      player2.scores.reduce((acc, curr) => acc + curr, 0)
+    );
+  }
+  checkTotalScore(
+    player1.scores.reduce((a: number, b: number) => a + b, 0),
+    player2.scores.reduce((a: number, b: number) => a + b, 0)
+  );
 }
 
-function checkTotalScore(p1Score:number, p2Score:number){
-  if(p1Score >= 100){
-    document.querySelector('#modal')?.classList.add('!scale-100')
+function checkTotalScore(p1Score: number, p2Score: number) {
+  if (p1Score >= 100) {
+    document.querySelector("#modal")?.classList.add("!scale-100");
     firstPlayerButton.disabled = true;
-    let playerWin = document.querySelector('#playerWin')
-    if(playerWin){
-      playerWin.textContent = `Player ${player1.playerName} wins! 🥳`
+    let playerWin = document.querySelector("#playerWin");
+    if (playerWin) {
+      playerWin.textContent = `Player ${player1.playerName} wins! 🥳`;
     }
   }
-  if(p2Score >= 100){
-    document.querySelector('#modal')?.classList.add('!scale-100')
+  if (p2Score >= 100) {
+    document.querySelector("#modal")?.classList.add("!scale-100");
     secondPlayerButton.disabled = true;
-    let playerWin = document.querySelector('#playerWin')
-    if(playerWin){
-      playerWin.textContent = `Player ${player2.playerName} wins! 🥳`
+    let playerWin = document.querySelector("#playerWin");
+    if (playerWin) {
+      playerWin.textContent = `Player ${player2.playerName} wins! 🥳`;
     }
-    }
+  }
 }
 
-function restartGame(){
-  document.querySelector('#modal')!.classList.remove('!scale-100')
+function restartGame() {
+  document.querySelector("#modal")!.classList.remove("!scale-100");
   player1.scores = [];
   player2.scores = [];
-  
-  document.querySelector('#showScoreP1')!.textContent = '';
-  document.querySelector('#showScoreP2')!.textContent = '';
-  document.querySelector('#totalScore')!.textContent = '0';
-  document.querySelector('#totalScoreP2')!.textContent = '0';
+
+  document.querySelector("#showScoreP1")!.textContent = "";
+  document.querySelector("#showScoreP2")!.textContent = "";
+  document.querySelector("#totalScore")!.textContent = "0";
+  document.querySelector("#totalScoreP2")!.textContent = "0";
   firstPlayerButton.disabled = false;
   secondPlayerButton.disabled = true;
 }
